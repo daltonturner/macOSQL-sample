051f2d50320b1b0c28d81b90e263b2c39d9be7d6
Dalton Turner <turnerdalton055@gmail.com>
2022-05-29 15:12:15 -0700

    Initial Commit

---
+++ macOSQL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  macOSQL
+//
+//  Created by Dalton Turner on 5/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macOSQL/macOSQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOSQLApp.swift
+//  macOSQL
+//
+//  Created by Dalton Turner on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct macOSQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
